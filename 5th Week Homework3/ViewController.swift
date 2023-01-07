a38a4b8d1a4b959da14aec93a7d6f427ffb53c85
Murat Ceyhun KORPEOGLU <muratceyhunkorpeoglu@gmail.com>
2023-01-08 00:12:14 +0300

    Initial Commit

---
+++ 5th Week Homework3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  5th Week Homework3
+//
+//  Created by Murat Ceyhun Korpeoglu on 8.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
